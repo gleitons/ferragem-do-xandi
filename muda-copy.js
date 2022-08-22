dba2c121167858a20a66ab6738499f414749ef79
Brasil Gondolas <gleitons@icloud.com>
2022-08-22 09:39:55 -0300

    Site Xa

---
+++ muda-copy.js
@@ -0,0 +1,16 @@
+document.querySelector('.copyright-text').innerHTML = `<p><a href="http://seusiteonline.com/" target="_blank">© 2021 | Seu Site Online</a></p>`;
+
+document.querySelector('.top-header-text').innerHTML = `<p>R. Pastor Gustav Nordlund, 337 - São José, Novo Hamburgo | ferragemdoxandi@gmail.com | 51 99545.8366 </p>
+<!-- / 51 3556.0298 -->`;
+
+document.querySelector('.social-links').innerHTML = `<li>
+<a href="https://www.facebook.com/ferragem.doxandi.3" class="facebook"><i class="socmed socmed-facebook"></i></a>
+</li>
+                            <li>
+<a href="mailto:ferragemdoxandi@gmail.com" class="email"><i class="socmed socmed-email"></i></a>
+</li>`;
+
+
+
+
+
