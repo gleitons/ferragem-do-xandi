8c5602006af3a558a1c5db4b27c76326126a402b
Gleiton Soares de Souza <31367377+gleitons@users.noreply.github.com>
2023-09-06 17:58:10 -0300

    atualização

--- muda-copy.js
+++ muda-copy.js
@@ -1,4 +1,4 @@
-document.querySelector('.copyright-text').innerHTML = `<p><a href="http://seusiteonline.com/" target="_blank">© 2022 | Seu Site Online</a></p>`;
+document.querySelector('.copyright-text').innerHTML = `<p><a href="http://gleiton.com.br/servicos" target="_blank">© 2022 | Futuro Consultoria</a></p>`;
 
 document.querySelector('.top-header-text').innerHTML = `<p>R. Pastor Gustav Nordlund, 337 - São José, Novo Hamburgo | ferragemdoxandi@gmail.com | 51 99545.8366  / 51 3556.0298</p>
 <!-- / 51 3556.0298 -->`;
@@ -9,6 +9,11 @@ document.querySelector('.social-links').innerHTML = `<li>
                             <li>
 <a href="mailto:ferragemdoxandi@gmail.com" class="email"><i class="socmed socmed-email"></i></a>
 </li>`;
+
+
+const todoCorpo = document.body.innerHTML
+const substituiCorpo = todoCorpo.replace(/https:\/\/ferragemxandi.com.br/gi, '')
+document.querySelector('body').innerHTML = substituiCorpo
 
 
 
